24e766a0b75cd9fa915b43c7166f9754a09c396a
dbm-ori-user-1 <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>
2021-06-30 19:24:34 +0000

    COMMIT Table_1

--- dbo/Table/Table_1.sql
+++ dbo/Table/Table_1.sql
@@ -3,8 +3,7 @@ CREATE TABLE [dbo].[Table_1] (
     [col2] NCHAR (10) NULL,
     [col3] NCHAR (10) NULL,
     [col4] NCHAR (10) NULL,
-    [col5] NCHAR (10) NULL,
-    [COL6] NCHAR (10) NULL
+    [col5] NCHAR (10) NULL
 );
 
 
