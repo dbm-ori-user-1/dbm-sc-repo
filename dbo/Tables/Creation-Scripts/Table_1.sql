29d349a0bbc87f578a213d450d3d2676e170367b
dbm-ori-user-1 <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>
2021-07-12 18:41:42 +0000

    COMMIT and PUSH Table changes

---
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[Table_1] (
+    [id1]  NCHAR (10) NULL,
+    [col2] NCHAR (10) NULL,
+    [col3] NCHAR (10) NULL,
+    [col4] NCHAR (10) NULL,
+    [col5] NCHAR (10) NULL,
+    [col6] NCHAR (10) NULL
+);
+
+
+GO
+
---
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Table_2] (
+    [col_1] NCHAR (10) NULL,
+    [col_2] NCHAR (10) NULL
+);
+
+
+GO
+
